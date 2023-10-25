39c3d6c1fac82984af2cbac8558102d8ceeb318c
Toni Hukka <berendin@gmail.com>
2023-10-25 19:43:21 +0300

    added interfaces and types

--- src/GameFetcher.tsx
+++ src/GameFetcher.tsx
@@ -4,26 +4,52 @@ import { useState, useEffect } from 'react'
 /* https://statsapi.web.nhl.com/api/v1/schedule?date=2023-10-20 */
 // yyyy-mm-dd
 
+interface GamePks {
+    totalGames: number,
+    gamePks: Array<number>
+}
 
-function GameFetcher(): void {
-    const [games, setGames] = useState(null)
+interface ReturnedApiObject {
+    copyright: string,
+    totalItems: number,
+    totalEvents: number,
+    totalGames: number,
+    totalMatches: number,
+    metaData: object,
+    wait: number,
+    dates: {
+        totalGames: number,
+        games: {
+            gamePk: number
+        }[]
+    }[]
+}
 
+type ApiResponse = ReturnedApiObject | null
+
+
+function GameFetcher(): GamePks {
+    const [games, setGames] = useState<ApiResponse>(null)
     // gets the current day in YYYY-MM-DD format
     const currentDay: string = new Date().toISOString().slice(0,10)
     console.log('currentDay: ', currentDay)
-
-    // gamePK is needed from here
-    // dates (array) -> games (array) -> index.gamePK
-    // dates.totalGames useful too
+    
+    // get list of games from nhl api
     useEffect(() => {
         fetch(`https://statsapi.web.nhl.com/api/v1/schedule?date=2023-10-20`)
         /* fetch(`https://statsapi.web.nhl.com/api/v1/schedule?date=${currentDay}`) */
         .then(res => res.json())
         .then(data => setGames(data))
     }, [])
-
+    
     let gamesArr;
-    const pkList = [];
+    
+    console.log(games)
+    const pkList: Array<number> = [];
+    let fetchedGamePks = {
+        totalGames: 0,
+        gamePks: pkList
+    };
 
     if (games == null) {
         console.log('games var null')
@@ -32,10 +58,15 @@ function GameFetcher(): void {
         gamesArr = games.dates[0].games
 
         gamesArr.forEach(i => pkList.push(i.gamePk))
+
+        fetchedGamePks = {
+            totalGames: games.dates[0].totalGames,
+            gamePks: pkList
+        }
     }
 
-    console.log('pkList: ', pkList)
-
+    console.log('fetchedGamePks: ', fetchedGamePks)
+    return fetchedGamePks
 }
 
 export default GameFetcher
